40248712fb9db445d0968a89676484eab557a745
tobe <tobegit3hub@gmail.com>
2013-12-23 21:59:33 +0800

    indent pycss.py code

--- src/pycss.py
+++ src/pycss.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 ''' convert .pcss into .css '''
 
@@ -7,52 +7,50 @@ import sys
 
 def main():
 	
-	pcssFileName = sys.argv[1]
-	if not pcssFileName.endswith('.pcss'):
-		print("Not a pcss file, exiting...")
-		return False
-	pcssFile = open(pcssFileName, 'r')
-
-	cssFileName = pcssFileName[:-5] + '.css'
-	cssFile = open(cssFileName, 'w')
-
-	try:
-		isInBlock = False
-
-		for line in pcssFile:
-			
-			# empty line
-			if not line.split(): 
-				newLine = '\n'
-				if isInBlock:
-					newLine = '}' + '\n' + '\n'
-					isInBlock = False
-
-			# comment line
-			elif line.startswith('#'):
-				newLine = '/* ' + line[1:-1] +' */' + '\n'			
-
-			# selector line
-			elif line.split() and not line.startswith('\t'): 
-				newLine = line.strip() + '{' + '\n'
-				isInBlock = True
-
-			# attribute line
-			elif line.startswith('\t'): 
-				newLine = '\t' + line.strip() + ';' + '\n'
-
-
-			cssFile.write(newLine)
-
-		if isInBlock:
-			newLine = '}' + '\n'
-			cssFile.write(newLine)
-
-	except IOError as e:
-		print(e)
-	finally:
-		pcssFile.close()
-		cssFile.close()
-
+  pcssFileName = sys.argv[1]
+  if not pcssFileName.endswith('.pcss'):
+    print("Not a pcss file, exiting...")
+    return False
+  pcssFile = open(pcssFileName, 'r')
+  
+  cssFileName = pcssFileName[:-5] + '.css'
+  cssFile = open(cssFileName, 'w')
+  
+  try:
+    isInBlock = False
+    
+    for line in pcssFile:
+	    
+      # empty line
+      if not line.split(): 
+        newLine = '\n'
+	if isInBlock:
+          newLine = '}' + '\n' + '\n'
+	  isInBlock = False
+	
+	# comment line
+	elif line.startswith('#'):
+          newLine = '/* ' + line[1:-1] +' */' + '\n'			
+	
+	# selector line
+	elif line.split() and not line.startswith('\t'): 
+          newLine = line.strip() + '{' + '\n'
+	  isInBlock = True
+	  
+	# attribute line
+	elif line.startswith('\t'): 
+          newLine = '\t' + line.strip() + ';' + '\n'
+	  cssFile.write(newLine)
+	  
+    if isInBlock:
+      newLine = '}' + '\n'
+      cssFile.write(newLine)
+	    
+  except IOError as e:
+    print(e)
+  finally:
+    pcssFile.close()
+    cssFile.close()
+    
 if __name__ == '__main__':
-	main()
+  main()
